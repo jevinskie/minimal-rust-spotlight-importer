6a232e3072c4289cbcaccd5a8eadb8bd6d3cd5c3
Jevin Sweval <jevinsweval@gmail.com>
2025-04-22 11:35:26 -0400

    oh no i've been segfaulting this whole time

--- minimal-importer-bundle/src/lib.rs
+++ minimal-importer-bundle/src/lib.rs
@@ -3,6 +3,7 @@
 // #![feature(no_sanitize)]
 // #![feature(box_as_ptr)]
 #![allow(non_snake_case)]
+#![allow(non_upper_case_globals)]
 
 use log::{LevelFilter, info};
 use objc2_core_foundation::{
@@ -15,7 +16,7 @@ use std::ptr;
 use std::ptr::NonNull;
 
 /// This is the key Spotlight expects for the human-readable description.
-const KMD_ITEM_DESCRIPTION: &str = "kMDItemDescription";
+const kMDItemDescription: &str = "kMDItemDescription";
 
 fn kMDImporterTypeID() -> CFRetained<CFUUID> {
     CFUUID::constant_uuid_with_bytes(
@@ -192,11 +193,29 @@ impl MetadataImporterPluginType {
             .query_interface_safe(self, iid, out)
     }
     pub fn add_ref(&mut self) -> ULONG {
+        println!("MetadataImporterPluginType::add_ref self: {self:#?}");
         self.refCount.checked_add(1).unwrap()
     }
     pub fn release(&mut self) -> ULONG {
-        panic!("unimplemented");
-        0
+        println!("MetadataImporterPluginType::release self: {self:#?}");
+        if self.refCount < 1 {
+            panic!("ref count underflow");
+        }
+        let rc = self.refCount.checked_sub(1).unwrap();
+        if rc != 0 {
+            rc
+        } else {
+            let fuuid = unsafe { CFRetained::from_raw(NonNull::new(self.factoryID).unwrap()) };
+            self.factoryID = ptr::null_mut();
+            let mut opt_ref_istruct = unsafe { self.conduitInterface.as_ref() };
+            let ref_istruct = opt_ref_istruct.as_mut().unwrap();
+            let ptb = unsafe { Box::from_raw(ref_istruct) };
+            println!("MetadataImporterPluginType::release drop ptb: {self:#?} ptb: {ptb:#?}");
+            drop(ptb);
+            CFPlugIn::remove_instance_for_factory(Some(&fuuid));
+            drop(fuuid);
+            rc
+        }
     }
 }
 
@@ -215,64 +234,10 @@ extern "C-unwind" fn com_add_ref(this: *mut MetadataImporterPluginType) -> ULONG
     unsafe { this.as_mut() }.unwrap().add_ref()
 }
 
-fn com_add_ref_safe(this: &mut MetadataImporterPluginType) -> ULONG {
-    println!("com_add_ref_safe begin this: {this:#?}");
-    if this.refCount < 1 {
-        panic!("ref count underflow");
-    }
-    this.refCount = this.refCount.checked_add(1).unwrap();
-    println!("com_add_ref_safe end this: {this:#?}");
-    this.refCount as ULONG
-}
-
 extern "C-unwind" fn com_release(this: *mut MetadataImporterPluginType) -> ULONG {
     let pt = &mut unsafe { *this };
     println!("com_release this: {this:#?} pt: {pt:#?}");
-    if pt.refCount < 1 {
-        panic!("ref count underflow");
-    }
-    pt.refCount = pt.refCount.checked_sub(1).unwrap();
-    if pt.refCount != 0 {
-        println!("com_release end (no-dealloc) this: {this:#?} pt: {pt:#?}");
-        pt.refCount as ULONG
-    } else {
-        println!("com_release end (dealloc) this: {this:#?} pt: {pt:#?}");
-        let fuuid = unsafe { CFRetained::from_raw(NonNull::new(pt.factoryID).unwrap()) };
-        pt.factoryID = ptr::null_mut();
-        let ptb = unsafe { Box::from_raw(this) };
-        println!("com_release drop ptb: {this:#?} pt: {pt:#?} ptb: {ptb:#?}");
-        drop(ptb);
-        println!("com_release call remove_instance_for_factory for {fuuid:#?}");
-        CFPlugIn::remove_instance_for_factory(Some(&fuuid));
-        println!("com_release drop fuuid: {fuuid:#?}");
-        drop(fuuid);
-        0
-    }
-}
-
-extern "C-unwind" fn com_release2(this: *mut MetadataImporterPluginType) -> ULONG {
-    let pt = &mut unsafe { *this };
-    println!("com_release this: {this:#?} pt: {pt:#?}");
-    if pt.refCount < 1 {
-        panic!("ref count underflow");
-    }
-    pt.refCount = pt.refCount.checked_sub(1).unwrap();
-    if pt.refCount != 0 {
-        println!("com_release end (no-dealloc) this: {this:#?} pt: {pt:#?}");
-        pt.refCount as ULONG
-    } else {
-        println!("com_release end (dealloc) this: {this:#?} pt: {pt:#?}");
-        let fuuid = unsafe { CFRetained::from_raw(NonNull::new(pt.factoryID).unwrap()) };
-        pt.factoryID = ptr::null_mut();
-        let ptb = unsafe { Box::from_raw(this) };
-        println!("com_release drop ptb: {this:#?} pt: {pt:#?} ptb: {ptb:#?}");
-        drop(ptb);
-        println!("com_release call remove_instance_for_factory for {fuuid:#?}");
-        CFPlugIn::remove_instance_for_factory(Some(&fuuid));
-        println!("com_release drop fuuid: {fuuid:#?}");
-        drop(fuuid);
-        0
-    }
+    unsafe { this.as_mut() }.unwrap().release()
 }
 
 extern "C-unwind" fn com_importer_import_data(
@@ -295,7 +260,7 @@ extern "C-unwind" fn com_importer_import_data(
     //     let mut first_line = String::new();
     //     if reader.read_line(&mut first_line).is_ok() {
     //         let desc_cfstr = CFString::from_str(first_line.trim());
-    //         let key = CFString::from_str(KMD_ITEM_DESCRIPTION);
+    //         let key = CFString::from_str(kMDItemDescription);
     //         // attr = desc_cfstr;
     //         // unsafe {objc2_core_foundation::CFDictionarySetValue(Some(&attr), key, &c_void::from(desc_cfst.)) };
     //         // unsafe { objc2_core_foundation::CFDictionarySetValue(attr as _, key.as_concrete_TypeRef() as _, desc_cfstr.as_concrete_TypeRef() as _) };
@@ -303,7 +268,7 @@ extern "C-unwind" fn com_importer_import_data(
     //     }
     // }
     if true {
-        let key = CFString::from_static_str(KMD_ITEM_DESCRIPTION);
+        let key = CFString::from_static_str(kMDItemDescription);
         let val = CFString::from_static_str("this is GREAT");
         CFMutableDictionary::<CFString, CFString>::add(
             unsafe {
